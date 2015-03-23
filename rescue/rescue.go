3966d7f42bafd59aa8938c0e1f7d649d619d9943
Bert Baron <bertusbaron@gmail.com>
2015-03-23 23:12:57 +0100

    Implemented up to bf search from single point

--- rescue/rescue.go
+++ rescue/rescue.go
@@ -25,6 +25,8 @@ var matrix []byte
 // set of people positions
 var ps map[int]bool
 
+var ops = 0
+
 func pack(x, y int) int {
 	return x*m + y
 }
@@ -33,6 +35,50 @@ func unpack(i int) (x, y int) {
 	x = i / m
 	y = i % m
 	return
+}
+
+func isValid(x, y int) bool {
+	return 0 <= x && x < n && 0 <= y && y < m
+}
+
+func expand(pos int) []int {
+	x, y := unpack(pos)
+	expanded := make([]int, 0, 8)
+	for i := x - 1; i <= x+1; i++ {
+		for j := y - 1; j <= y+1; j++ {
+			if (i != x || j != y) && isValid(i, j) {
+				neighbour := pack(i, j)
+				if matrix[neighbour] & U == 0 {
+					expanded = append(expanded, neighbour)
+				}
+			}
+		}
+	}
+	return expanded
+}
+
+func calculatePaths(pos int) {
+	type node struct {
+		p      int
+		parent *node
+	}
+	queue := make([]node, m*n)
+	head := 0
+	tail := 1
+	queue[head] = node{pos, nil}
+	for head < tail {
+		ops += 1
+		path := queue[head]
+		head += 1
+		expanded := expand(path.p)
+		for _, nb := range expanded {
+			if matrix[nb] & visited == 0 {
+				matrix[nb] |= visited
+				queue[tail] = node{nb, &path}
+				tail += 1
+			}
+		}
+	}
 }
 
 func parseInt(s string) int {
@@ -75,25 +121,34 @@ func main() {
 	// parse people
 	p = parseInt(nextLine(scanner))
 	ps = make(map[int]bool)
-	for i:=0 ; i<p; i++ {
+	for i := 0; i < p; i++ {
 		pos := parsePos(nextLine(scanner))
 		ps[pos] = true
-		matrix[pos] &= P
+		matrix[pos] |= P
 	}
 
 	// parse unpassable objects
 	u = parseInt(nextLine(scanner))
-	for i:=0 ; i<u; i++ {
+	for i := 0; i < u; i++ {
 		pos := parsePos(nextLine(scanner))
-		matrix[pos] &= U
+		matrix[pos] |= U
 	}
 
 	// parse exits
 	e = parseInt(nextLine(scanner))
-	for i:=0 ; i<e; i++ {
+	for i := 0; i < e; i++ {
 		pos := parsePos(nextLine(scanner))
-		matrix[pos] &= E
+		matrix[pos] |= E
 	}
-	
+
+	expanded := expand(pack(1, 0))
+	fmt.Printf("Expand: ")
+	for _, p := range expanded {
+		x, y := unpack(p)
+		fmt.Printf(" (%d,%d)", x, y)
+	}
+	fmt.Println("")
+	calculatePaths(0)
+	fmt.Printf("%d operations", ops)
 	fmt.Println("Done...")
 }
